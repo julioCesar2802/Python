8b251b48bfe532dcaf4147ddc7c88fa9505b2f2e
Júlio César Duarte de Moura <juliocesarduarte2021@gmail.com>
2026-01-26 20:57:32 +0000

    exercicios de beecrowd

---
+++ beecrowd/NumerosPositivos.py
@@ -0,0 +1,7 @@
+
+lista = []
+for i in range (1,7):
+    a = float(input())
+    if a > 0:
+        lista.append(a)
+print(f"{len(lista)} valores positivos")
---
+++ beecrowd/mes.py
@@ -0,0 +1,5 @@
+a = int(input())
+
+lista_mes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print(lista_mes[a - 1])
